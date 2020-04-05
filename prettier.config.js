fa2854a76cdb3ca24c7d642e50b2df56994d19ea
Norbert Korczyk <Norbert_Korczyk@epam.com>
2020-04-05 13:25:25 +0200

    coronavirus map

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 100,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+};
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Container from 'components/Container';
+
+const Header = () => (
+  <header>
+    <Container type="content">
+      <p>Coronavirus Stats</p>
+    </Container>
+  </header>
+);
+
+export default Header;
